bd3ec0aa2bd3163b3be47ca81a74bbf8846401a8
Abdulsalam Oetayo <adeodetayo92@gmail.com>
2021-12-27 13:44:35 -0500

    Feat: Create main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
 import styled from "styled-components";
+import PageNotFound from "./pages/404page";
+import { Route, Routes } from "react-router-dom";
+
+const HomePage = React.lazy(() => import("./pages/Home/homepage"));
 
 const Dev = styled.div`
   text-align: center;
   height: fit-content;
   background-color: rgb(0, 128, 0);
   color: white;
-  font-family: 'Noto Sans regular';
+  font-family: "Noto Sans regular";
 `;
 
 function App() {
   return (
     <div className="App">
-      <Dev>
-        development
-      </Dev>
+      <Dev>development</Dev>
+      <Suspense fallback={<Loading/>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
 
 export default App;
+
+function Loading() {
+  return <div>Loading</div>;
+}
---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+export enum COLORS {
+  WHITE = "#FFFFFF",
+  LIGHT_BLUE = "#00AFEF",
+  DARK_BLUE = "#047AAC",
+  BLACK = "#000000",
+}
