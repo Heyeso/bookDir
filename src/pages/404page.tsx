3a4e4724dcc205aba76ce6141c26f4c29d88696d
Abdulsalam Oetayo <adeodetayo92@gmail.com>
2021-12-27 13:14:56 -0500

    Init: Homepage

---
+++ src/pages/404page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import styled from "styled-components";
+
+function PageNotFound() {
+  return (
+    <div>
+        Page not Found
+    </div>
+  );
+}
+
+export default PageNotFound;
